6ed52b188405ddaa096afb78e4c02696f04bc389
Nistha <nisthakajoria@gmail.com>
2024-07-06 13:13:17 +0530

    CART BACKEND CHANGES

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { addToCart,removeFromCart,getCart } from "../controllers/cartController"
-import { get } from "mongoose";
+import authMiddleware from "../middleware/auth";
+
+
 
 const cartRouter = express.Router();
 
-cartRouter.post("/add". addToCart)
-cartRouter.post("/remove", removeFromCart)
-cartRouter.post("/get",getCart)
+cartRouter.post("/add", authMiddleware,addToCart)
+cartRouter.post("/remove", authMiddleware,removeFromCart)
+cartRouter.post("/get",authMiddleware,getCart)
 
 export default  cartRouter;
